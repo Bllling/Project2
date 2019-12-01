736495c33b7a9975956b5e338e2c751dc12cb412
Bllling <1556629488@qq.com>
2019-12-01 15:03:47 +0800

    1

---
+++ src/biz/IOrderDetailBiz.java
@@ -0,0 +1,5 @@
+package biz;
+
+public interface IOrderDetailBiz {
+    int add(String  rorderid,Integer arid,Double cpuprice,Double motherboardprice,Double memoryprice,Double diskprice,Double ssdprice,Double sourceprice,Double graphicsprice,Double boxprice);
+}
---
+++ src/biz/impl/IOrderDetaiBizImpl.java
@@ -0,0 +1,19 @@
+package biz.impl;
+
+import biz.IOrderDetailBiz;
+import dao.DBHelper;
+import dao.IOrderDetailDao;
+import dao.impl.IOrderDetaiDaoImpl;
+
+public class IOrderDetaiBizImpl implements IOrderDetailBiz {
+
+	@Override
+	public int add(String rorderid, Integer arid, Double cpuprice, Double motherboardprice, Double memoryprice,
+			Double diskprice, Double ssdprice, Double sourceprice, Double graphicsprice, Double boxprice) {
+		IOrderDetailDao dao = new IOrderDetaiDaoImpl();
+		return dao.add(rorderid, arid, cpuprice, motherboardprice, memoryprice, diskprice, ssdprice, sourceprice, graphicsprice, boxprice);
+	}
+
+
+
+}
